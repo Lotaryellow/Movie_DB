8ae90ee208bea311266f324af1e03f88d9baeadf
Lotaryellow <Keorefuzz@gmail.com>
2023-11-10 16:24:29 +0300

    Pro-30: add interface to infoResult

--- src/types/movies.ts
+++ src/types/movies.ts
@@ -34,3 +34,52 @@ export interface SearchingMovie {
     posterUrlPreview: string | null
   ];
 }
+
+export interface Movie {
+  kinopoiskId: number | null;
+  kinopoiskHDId: string | null;
+  imdbId: string | null;
+  nameRu: string | null;
+  nameEng: string | null;
+  nameOriginal: string | null;
+  posterUrl: string | null;
+  posterUrlPreveiw: string | null;
+  coverUrl: string | null;
+  logoUrl: string | null;
+  reviewCount: number | null;
+  ratingCount: number | null;
+  ratingGoodReview: number | null;
+  ratingGoodReviewVoteCount: number | null;
+  ratingKinopoisk: number | null;
+  ratingKinopoiskVoteCount: number | null;
+  ratingImdb: number | null;
+  ratingImdbVoteCount: number | null;
+  ratingFilmCritics: number | null;
+  ratingFilmCriticsVoteCount: number | null;
+  ratingAwait: number | null;
+  ratingAwaitCount: number | null;
+  ratingRfCritics: number | null;
+  ratingRfCriticsVoteCount: number | null;
+  webUrl: string | null;
+  year: number | null;
+  filmLength: number | null;
+  slogan: string | null;
+  description: string | null;
+  shortDescription: string | null;
+  editorAnnotation: string | null;
+  isTicketsAvailable: boolean | null;
+  productionStatus: string | null;
+  type: string | null;
+  ratingMpaa: string | null;
+  ratingAgeLimits: string | null;
+  hasImax: boolean | null;
+  has3D: boolean | null;
+  lastSync: string | null;
+  countries: { country: string }[] | null;
+  genres: { genre: string }[] | null;
+  startYear: number | null;
+  endYear: number | null;
+  serial: boolean | null;
+  shortFilm: boolean | null;
+  completed: boolean | null;
+}
